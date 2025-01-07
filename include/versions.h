ede1d2f3eea66a1ed049949170077bc305f548c8
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2025-01-07 12:18:47 +0100

    Updated OFM-FileTransferModule and OGM-Common and re-release 0.1.

--- include/versions.h
+++ include/versions.h
@@ -1,14 +1,29 @@
 #pragma once
 
-#define MAIN_Version "dfeb1f4"
+#define MAIN_Version "0487853"
 #define KNX_Version "2.1.2+764900e"
-#define MODULE_Common_Version "1.2.0+6b29988"
+#define MODULE_Common_Version "1.2.0+6f467fb"
+#define MODULE_Common_Version_Major 1
+#define MODULE_Common_Version_Minor 2
+#define MODULE_Common_Version_Revision 0
 #define MODULE_Common_ETS 18
 #define MODULE_LogicModule_Version "3.5.2+9a1cfe1"
+#define MODULE_LogicModule_Version_Major 3
+#define MODULE_LogicModule_Version_Minor 5
+#define MODULE_LogicModule_Version_Revision 2
 #define MODULE_LogicModule_ETS 53
 #define MODULE_BinaryInput_Version "0.2.0+9fd5ad1"
+#define MODULE_BinaryInput_Version_Major 0
+#define MODULE_BinaryInput_Version_Minor 2
+#define MODULE_BinaryInput_Version_Revision 0
 #define MODULE_BinaryInput_ETS 2
 #define MODULE_VirtualButton_Version "0.5.0+e39365e"
+#define MODULE_VirtualButton_Version_Major 0
+#define MODULE_VirtualButton_Version_Minor 5
+#define MODULE_VirtualButton_Version_Revision 0
 #define MODULE_VirtualButton_ETS 5
-#define MODULE_FileTransferModule_Version "0.1.0+7c2f91c"
+#define MODULE_FileTransferModule_Version "0.1.0+2669301"
+#define MODULE_FileTransferModule_Version_Major 0
+#define MODULE_FileTransferModule_Version_Minor 1
+#define MODULE_FileTransferModule_Version_Revision 0
 #define MODULE_FileTransferModule_ETS 1
